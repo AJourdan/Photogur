26627a39550a512e8b149ca8445c38f22e249200
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-09 14:25:08 -0500

    Configuring root as pictures#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
   Rails.application.routes.draw do
-  get 'pictures' => 'pictures#index'
+    root "pictures#index"
+    get 'pictures' => 'pictures#index'
 
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
+    post 'pictures' => 'pictures#create'
+    get 'pictures/new' => 'pictures#new'
 
 
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
+    get 'pictures/:id' => 'pictures#show', as: 'picture'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
